71197c4b35ce9075a31a4aa4b76cee9403663ebb
llcfromhell <llc.from.hell@gmail.com>
2016-09-23 21:56:43 -0300

    aula 04

---
+++ threads/src/main/java/br/com/alura/lista/Principal.java
@@ -0,0 +1,26 @@
+package br.com.alura.lista;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Principal {
+	
+	public static void main(String[] args) throws InterruptedException {
+	
+		List<String> lista = new Vector<String>();
+		
+		for (int i = 0; i < 100; i++) {
+			Thread thread = new Thread(new TarefaAdicionarElemento(lista, i));
+			thread.start();
+		}
+		
+		Thread.sleep(2000);
+		
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(i + " - " + lista.get(i));
+		}
+		
+		
+	}
+
+}
