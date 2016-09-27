30f990e7c4ba176d85aaa0a67bf8eb1c58e94e56
llcfromhell <llc.from.hell@gmail.com>
2016-09-26 23:49:05 -0300

    aula 05

--- banheiro/src/main/java/br/com/alura/banheiro/Banheiro.java
+++ banheiro/src/main/java/br/com/alura/banheiro/Banheiro.java
@@ -1,54 +1,123 @@
 package br.com.alura.banheiro;
 
 public class Banheiro {
+
+	private boolean ehSujo = true;
 	
-	public void fazNumero1() {
+	public void limpa() {
 
+		bateNaPorta();
 		
-		synchronized (this) {
-		
-			System.out.println(getNome() + " entrando no banheiro");
-	        System.out.println(getNome() + " fazendo coisa rapida");
+	    synchronized (this) {
 
-	        try {
-	            Thread.sleep(8000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
+	    	entraNoBanheiro();
+	        
+	    	if (!this.ehSujo) {
+	            System.out.println(getNome() + ", não está sujo, vou sair");
+	            return;
 	        }
 
-	        msgEnd();
-			
-		}
-		
-        
-    }
+	        System.out.println(getNome() + " limpando o banheiro");
+	        
+	        this.ehSujo = false;
 
+	        descansaPor(10000);
+	        
+	        this.notifyAll();
+
+	        saiDoBanheiro();
+	    }
+	}
+
+	private void saiDoBanheiro() {
+		System.out.println(getNome() + " saindo do banheiro");
+	}
+	
+	private void esperaLaFora(String nome) {
+
+	    System.out.println(nome + ", eca, banheiro está sujo");
+	    try {
+	        this.wait();
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+	}
+		
 	private String getNome() {
 		return Thread.currentThread().getName();
 	}
+
+	public void fazNumero1() {
+
+		bateNaPorta();
+		
+		synchronized (this) {
+			
+			entraNoBanheiro();
+	        
+			verificaSujeira();
+			
+			System.out.println(getNome() + " fazendo coisa rapida");
+
+			suja();
+			
+	        descansaPor(3000);
+
+	        terminaESaiDoBanheiro();
+			
+		}
+        
+    }
 	
 	public void fazNumero2() {
 
+		bateNaPorta();
+		
 		synchronized (this) {
 		
-	        System.out.println(getNome() + " entrando no banheiro");
+	        entraNoBanheiro();
+	        
+	        verificaSujeira();
+	        
 	        System.out.println(getNome() + " fazendo coisa demorada");
 	
-	        try {
-	            Thread.sleep(15000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
+	        descansaPor(5000);
 	
-	        msgEnd();
+	        terminaESaiDoBanheiro();
 	        
 		}
     }
 
-	private void msgEnd() {
+	private void suja() {
+		this.ehSujo = true;
+	}
+	
+	private void bateNaPorta() {
+		System.out.println(getNome() + " batendo na porta");
+	}
+
+	private void entraNoBanheiro() {
+		System.out.println(getNome() + " entrando no banheiro");
+	}
+
+	private void verificaSujeira() {
+		while (this.ehSujo) {
+		    esperaLaFora(getNome());
+		}
+	}
+	
+	private void descansaPor(long millis) {
+		try {
+		    Thread.sleep(millis);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+	}
+
+	private void terminaESaiDoBanheiro() {
 		System.out.println(getNome() + " dando descarga");
         System.out.println(getNome() + " lavando a mao");
-        System.out.println(getNome() + " saindo do banheiro");
+        saiDoBanheiro();
 	}
 	
 		
